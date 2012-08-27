21b0c9b0a812de22fa4b43829452324fce01445b
Jonathan Martin <me@nybblr.com>
2012-08-27 12:46:08 -0400

    Skeleton rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omelet::Application.config.secret_token = '6dc8f9bf811f56e3caa553b88047f91198732e62e286d3e74fb9d7d724f4a242565871eafa645ec7cd5d957f480055c203bea85680f8c241e028f2437f92db44'
