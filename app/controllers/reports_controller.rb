c9a6e4b83544e20f75cd5a10d288b6554065e760
Jonathan Martin <me@nybblr.com>
2012-10-16 15:19:02 -0400

    Throw error when no app_id and user_id given. Also added show action.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,28 +1,33 @@
 class ReportsController < ApplicationController
 	respond_to :json
 
-  # DRY the param lookup
-  before_filter { @app_id = params[:app_id]
-                  @user_id = params[:user_id] }
+	# DRY the param lookup
+	before_filter {
+		@app_id = params[:app_id]
+		@user_id = params[:user_id]
 
-  # DRY up report lookup
-  # Scope report by app_id, user_id, then search by absolute ID
-  before_filter :except => [:index, :create] do
-    @report = Report.for(@app_id, @user_id).find(params[:id])
-  end
+		# Must give app_id and user_id!
+		throw ActiveRecord::RecordNotFound if @app_id.blank? or @user_id.blank?
+	}
+
+	# DRY up report lookup
+	# Scope report by app_id, user_id, then search by absolute ID
+	before_filter :except => [:index, :create] do
+		@report = Report.for(@app_id, @user_id).find(params[:id])
+	end
 
 	def index
-    @reports = Report.for(@app_id, @user_id)
+		@reports = Report.for(@app_id, @user_id)
 	end
 
 	def show
-
+		@report = Report.for(@app_id, @user_id).find(params[:id])
 	end
 
 	def create
-    @callback = params[:callback]
-    @query = params[:query]
-    @db = params[:db]
+		@callback = params[:callback]
+		@query = params[:query]
+		@db = params[:db]
 
 	end
 
@@ -30,7 +35,7 @@ class ReportsController < ApplicationController
 
 	end
 
-  def destroy
+	def destroy
 
-  end
+	end
 end
